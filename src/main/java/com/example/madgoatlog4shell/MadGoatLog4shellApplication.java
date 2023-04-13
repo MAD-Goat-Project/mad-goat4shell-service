01390554ca566ceb16070432cdbb51f36b60f9ae
Luís Ventuzelos <luisventuzelos@gmail.com>
2023-04-13 23:37:58 +0100

    First commit

---
+++ src/main/java/com/example/madgoatlog4shell/MadGoatLog4shellApplication.java
@@ -0,0 +1,13 @@
+package com.example.madgoatlog4shell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadGoatLog4shellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadGoatLog4shellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/madgoatlog4shell/MadGoatLog4shellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madgoatlog4shell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadGoatLog4shellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
